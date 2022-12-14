406ea62b16b86a84a1658a2a49457fe52747525b
Roshan <thaparoshan3722@gmail.com>
2022-12-14 19:47:18 +0545

    little update in html and js not managed properly

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ const PopUp=document.getElementById("Log-In-Pop-Up");
 
 LogIn.addEventListener("click",temp);
 
+const HomeRightContainer=document.querySelector("#Body-Container>#Right-Container");
+
+const HomeLeftContainer=document.querySelector("#Body-Container>#Left-Container");
+
+let i=0;
+
+console.log(HomeLeftContainer.children.length);
+
+for(i=0;i<HomeLeftContainer.children.length;i++)
+{
+    HomeLeftContainer.children[i].addEventListener('click',updateHomeNews);
+}
 
 function temp()
 {
@@ -21,4 +33,9 @@ function closePopUp()
 {
     PopUp.style.zIndex=-5;
     // alert("pop up closed");
+}
+
+function updateHomeNews()
+{
+    console.log(HomeRightContainer.children[i]);
 }
